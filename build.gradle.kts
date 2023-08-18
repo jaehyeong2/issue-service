884f068feb6764e99f6e26ef40e954fc00926b27
jaehyeong <wogud1514@gmail.com>
2023-08-18 21:54:17 +0900

    chore: kotlin logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    //logging
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
 }
 
 tasks.withType<KotlinCompile> {
