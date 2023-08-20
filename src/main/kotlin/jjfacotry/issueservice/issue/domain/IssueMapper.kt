b27f0416dfffb241694593943e1cff295f63a975
jaehyeong2 <wogud1514@gmail.com>
2023-08-20 14:44:08 +0900

    refactor: mapper return 시 with 사용

--- src/main/kotlin/jjfacotry/issueservice/issue/domain/IssueMapper.kt
+++ src/main/kotlin/jjfacotry/issueservice/issue/domain/IssueMapper.kt
@@ -5,13 +5,15 @@ import org.springframework.stereotype.Component
 @Component
 class IssueMapper {
     fun of(issue: Issue) : IssueInfo.Main{
-        return IssueInfo.Main(
-            userId = issue.userId,
-            summary = issue.summary,
-            description = issue.description,
-            type = issue.type,
-            priority = issue.priority,
-            status = issue.status
-        )
+        with(issue){
+            return IssueInfo.Main(
+                userId = userId,
+                summary = summary,
+                description = description,
+                type = type,
+                priority = priority,
+                status = status
+            )
+        }
     }
 }
